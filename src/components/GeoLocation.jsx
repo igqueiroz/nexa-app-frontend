1a4610e1accafeb5e21eddc496364fcf5cf589b9
Igor Queiroz <>
2019-04-02 20:56:12 -0300

    Added Logo, GeoLocation and About Texts

--- src/components/GeoLocation.jsx
+++ src/components/GeoLocation.jsx
@@ -3,10 +3,22 @@ import styled from "styled-components"
 
 export default function GeoLocation(props) {
     return (
-        <div className="container">
+        <div className="container mt-5">
             <div className="row">
                 <div className="col-md-12">
-                    geolocation
+                    <section>
+						<div className="row">
+							<div className="col-sm-1" />
+							<div className="col-sm-10">
+								<h1>O que é Geolocalização?</h1>
+								<p>Geolocalização é um detecção sem fio da localização física de um dispositivo remoto. Como substantivo, a geolocalização refere-se à localização física em si. Como verbo, o termo refere-se ao processo de detecção desse local.
+								</p>
+								<p>Dependendo do método usado, uma geolocalização pode ser tão geral quanto o continente ao qual o usuário está se conectando ou tão específico quanto sua posição geográfica, definida como coordenadas de latitude e longitude, ou às vezes latitude, longitude e altitude. A localização é um atributo menos específico, como, por exemplo, o Central Park de Nova York ou o North End de Boston.
+								</p>
+							</div>
+							<div className="col-sm-1" />
+						</div>
+					</section>
                 </div>
             </div>
         </div>
