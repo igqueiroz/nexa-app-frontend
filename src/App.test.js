a4d7d5ad9491d04fe5cb84fa652cf0cf85b1cdb4
Igor Queiroz <>
2019-04-05 09:33:35 -0300

    >> Refactoring Home >> Performance improved >> Maps Validation fixed >> Jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('Renderiza sem quebrar elementos', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+});
+
+it('Verifica se o serviço que liga no banco do App Engine está respondendo', () => {
+	fetch(`https://mongo-dot-nexa-digital.appspot.com/userlist`).then(response => {
+		if(response.ok) {
+			return true
+		}
+		else {
+			return false
+		}
+	})
 });
